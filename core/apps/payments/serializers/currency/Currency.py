7d9d65499fd0d14e243c8748f40d4bb8d30db89c
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-01-25 15:05:07 +0500

    ` "BaseCurrencySerializerda status maydoni qo'shildi va exclude joyi fieldsga o'zgartirildi"`

--- core/apps/payments/serializers/currency/Currency.py
+++ core/apps/payments/serializers/currency/Currency.py
@@ -4,12 +4,28 @@ from ...models import CurrencyModel
 
 
 class BaseCurrencySerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
     class Meta:
         model = CurrencyModel
-        exclude = [
-            "created_at",
-            "updated_at",
+        fields = [
+            "id", 
+            "code", 
+            "symbol", 
+            "no_of_decimal", 
+            "exchange_rate", 
+            "symbol_position", 
+            "thousands_separator", 
+            "decimal_separator", 
+            "system_reserve", 
+            "status", 
+            "created_by_id", 
+            "created_at", 
+            "updated_at", 
+            "deleted_at"
         ]
+        
+    def get_status(self, obj):
+        return 1 if obj.status else 0
 
 
 class ListCurrencySerializer(BaseCurrencySerializer):
