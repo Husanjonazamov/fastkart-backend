1a7301f1c8860bf6a8ad7c1154361652f9caad9b
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-01-28 18:48:39 +0500

    Savollarni olish va yaratish serializeriga consumer_id va product_id qo'shildi

--- core/apps/content/serializers/questions/questions.py
+++ core/apps/content/serializers/questions/questions.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from ...models import QuestionsModel
+from core.apps.accounts.models.user import User
+from core.apps.product.models.product import ProductModel
 
 
 class BaseQuestionsSerializer(serializers.ModelSerializer):
@@ -34,5 +36,34 @@ class RetrieveQuestionsSerializer(BaseQuestionsSerializer):
     class Meta(BaseQuestionsSerializer.Meta): ...
 
 
+from rest_framework.exceptions import NotFound
+
 class CreateQuestionsSerializer(BaseQuestionsSerializer):
-    class Meta(BaseQuestionsSerializer.Meta): ...
+    consumer_id = serializers.IntegerField(write_only=True, required=False)
+    product_id = serializers.IntegerField(write_only=True, required=False)
+
+    class Meta(BaseQuestionsSerializer.Meta):
+        fields = BaseQuestionsSerializer.Meta.fields + ['consumer_id', 'product_id']
+
+    def create(self, validated_data):
+        consumer_id = validated_data.pop('consumer_id', None)
+        product_id = validated_data.pop('product_id', None)
+
+        consumer = User.objects.get(id=consumer_id) if consumer_id else None
+
+        try:
+            product = ProductModel.objects.get(id=product_id) if product_id else None
+        except ProductModel.DoesNotExist:
+            raise NotFound(f"Product with ID {product_id} does not exist.")
+
+        question = QuestionsModel.objects.create(
+            question=validated_data['question'],
+            answer=validated_data['answer'],
+            consumer=consumer,
+            product=product,
+            reaction=validated_data.get('reaction', None),
+            total_likes=validated_data.get('total_likes', 0),
+            total_dislikes=validated_data.get('total_dislikes', 0),
+            deleted_at=validated_data.get('deleted_at', None),
+        )
+        return question
--- core/apps/content/views/questions.py
+++ core/apps/content/views/questions.py
@@ -3,14 +3,14 @@ from typing import Any
 from django_core.mixins import BaseViewSetMixin
 from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from ..models import QuestionsModel
 from ..serializers.questions import CreateQuestionsSerializer, ListQuestionsSerializer, RetrieveQuestionsSerializer
 
 
 @extend_schema(tags=["questions"])
-class QuestionsView(BaseViewSetMixin, ReadOnlyModelViewSet):
+class QuestionsView(BaseViewSetMixin, ModelViewSet):
     queryset = QuestionsModel.objects.all()
 
     def get_serializer_class(self) -> Any:
